b35067856d029c1076309e96659180e6f07c6f24
Lisa <56881057+lisaalexs@users.noreply.github.com>
2021-09-01 16:37:53 +0200

    Update test.js

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -3,12 +3,12 @@ describe("My First Test", () => {
     cy.visit('http://localhost:8080');
   });
   it("input data", () => {
-    cy.get("[newTodo-input]")
+    cy.get("#newTodoInput")
       .type("Walk the dog")
       .should("have.value", "Walk the dog");
   });
   it("checks and unchecks checkboxes", () => {
-    cy.get("[newTodo-input]").type("{enter}");
+    cy.get("#newTodoInput").type("{enter}");
     cy.get("#checkboxLabel").click();
     cy.get("label > :checkbox ").check();
     cy.wait(3000);
@@ -17,12 +17,12 @@ describe("My First Test", () => {
   });
   it("adds a new todo", () => {
     cy.get(".addTodo").click();
-    cy.get("[newTodo-input]").type("Go to the gym").type("{enter}");
+    cy.get("#newTodoInput").type("Go to the gym").type("{enter}");
   });
   it("filter todos", () => {
-    cy.get("[newTodo-input]").type("Water flowers").type("{enter}");
-    cy.get("[newTodo-input]").type("Do homework").type("{enter}");
-    cy.get("[newTodo-input]").type("Write some code").type("{enter}");
+    cy.get("#newTodoInput").type("Water flowers").type("{enter}");
+    cy.get("#newTodoInput").type("Do homework").type("{enter}");
+    cy.get("#newTodoInput").type("Write some code").type("{enter}");
     cy.get("#checkboxLabel").first().click();
     cy.get("label > :checkbox").first().check();
     cy.get("label > :radio").should("have.length", 3);
